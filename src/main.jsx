21c8b10b03bd1b76c789728d54e42b069fee6c2d
wwwchendev <wwwchendev@gmail.com>
2024-02-08 19:05:33 +0800

    路由設置

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import App from '@/components/App';
 import './index.css';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <p>children el</p>,
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+  <RouterProvider router={router} />
+)
