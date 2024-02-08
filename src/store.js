2829bd64628484fb8b0f47f5a12226461c5001f4
wwwchendev <wwwchendev@gmail.com>
2024-02-08 19:20:57 +0800

    設計store結構

---
+++ src/store.js
@@ -0,0 +1,18 @@
+// createStore 用於創建 Redux store，並將 reducer 作為參數傳遞給它。
+// 這個 store 實例可以使用 dispatch 方法來派發 action，
+// 從而觸發 reducer 函數，進而改變應用程序的狀態。這是 Redux 中管理狀態的一個基本流程。
+
+/*
+1.設計store結構
+  {
+    task:[
+      {
+        id:1,
+        task:'design store',
+        completed: false,
+      },{...},{...}
+    ],
+    employees: [{...},{...},{...}]
+  }
+*/
+
