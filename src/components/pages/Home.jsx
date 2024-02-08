fa47c5dd78c01390cadf1407169b3a864d5b4721
wwwchendev <wwwchendev@gmail.com>
2024-02-08 20:25:24 +0800

    用subscribe()訂閱store異動與取消訂閱

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import store from "@/store"
 import { addTask, removeTask } from '@/actions'
 
-store.dispatch(addTask('這是一個新任務2'))
+// 訂閱狀態更新
+const unsubscribe = store.subscribe(() => {
+  console.log('更新', store.getState());
+})
+
+// 派發action
+store.dispatch(addTask('這是一個新任務1'))
+// 取得狀態
 console.log(JSON.stringify(store.getState()));
 // >>[{"id":1,"task":"這是一個新任務2","completed":false}]
+
+// 取消訂閱
+unsubscribe()
 
 store.dispatch(removeTask(1))
 console.log(JSON.stringify(store.getState()));
 // >>[]
+
 
 const Home = () => {
   return (
