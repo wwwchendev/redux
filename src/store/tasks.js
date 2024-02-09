9bf3be891421db5d5751e4205fa513a7724bedef
wwwchendev <wwwchendev@gmail.com>
2024-02-09 16:57:22 +0800

    使用createSlice建立action creatords和reducer

--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,9 +1,30 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-// createAction
-export const addTask = createAction('ADD_TASK');
-export const removeTask = createAction('REMOVE_TASK');
-export const completeTask = createAction('COMPLETE_TASK');
+// 使用createSlice可以同時創建Action和Reducer
+const taskSlice = createSlice({
+  name: 'tasks',
+  initialState: [],
+  reducers: {
+    // action: function
+    addTask: (state, action) => {
+      state.push({
+        id: state.length + 1,
+        task: action.payload.task,
+        completed: false,
+      });
+    },
+    removeTask: (state, action) => {
+      return state.filter(task => task.id !== action.payload.id);
+    },
+    completeTask: (state, action) => {
+      const index = state.findIndex(task => task.id === action.payload.id);
+      if (index !== -1) {
+        state[index].completed = true;
+      }
+    },
+  },
+});
+
 export const fetchTodo = id => {
   return async function (dispatch, getState) {
     try {
@@ -18,25 +39,33 @@ export const fetchTodo = id => {
   };
 };
 
+export const { addTask, removeTask, completeTask } = taskSlice.actions;
+export default taskSlice.reducer;
+
+// createAction
+// export const addTask = createAction('ADD_TASK');
+// export const removeTask = createAction('REMOVE_TASK');
+// export const completeTask = createAction('COMPLETE_TASK');
+
 // reducer
 // 創建reducer函數用於定義如何執行操作(How to do)
-const initialState = [];
-export default createReducer(initialState, builder => {
-  builder
-    .addCase(addTask.type, (state, action) => {
-      state.push({
-        id: state.length + 1,
-        task: action.payload.task,
-        completed: false,
-      });
-    })
-    .addCase(removeTask.type, (state, action) => {
-      return state.filter(task => task.id !== action.payload.id);
-    })
-    .addCase(completeTask.type, (state, action) => {
-      const index = state.findIndex(task => task.id === action.payload.id);
-      if (index !== -1) {
-        state[index].completed = true;
-      }
-    });
-});
+// const initialState = [];
+// export default createReducer(initialState, builder => {
+//   builder
+//     .addCase(addTask.type, (state, action) => {
+//       state.push({
+//         id: state.length + 1,
+//         task: action.payload.task,
+//         completed: false,
+//       });
+//     })
+//     .addCase(removeTask.type, (state, action) => {
+//       return state.filter(task => task.id !== action.payload.id);
+//     })
+//     .addCase(completeTask.type, (state, action) => {
+//       const index = state.findIndex(task => task.id === action.payload.id);
+//       if (index !== -1) {
+//         state[index].completed = true;
+//       }
+//     });
+// });
