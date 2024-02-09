7cce0f5f409c79bbb2474f6a5c41882f07166cb8
wwwchendev <wwwchendev@gmail.com>
2024-02-10 00:59:52 +0800

    使用axios(fetch)呼叫API

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,39 +1,35 @@
+import axios from '@/utils/http';
 import store from '@/store/configureStore';
-import { addTask, removeTask, completeTask, fetchTodo } from '@/store/tasks';
-import { addEmployee, removeEmployee } from '@/store/employees';
 
-// 訂閱狀態更新
-const unsubscribe = store.subscribe(() => {
-  console.log('更新', store.getState());
-});
+import { getTasks } from '@/store/tasks';
 
-// 派發action
-store.dispatch(addEmployee({ name: '員工一' }));
-store.dispatch(removeEmployee({ id: 1 }));
-// store.dispatch(addTask({ task: '使用createAction建立' }));
-// store.dispatch(completeTask({ id: 1 }));
-// >>[{"id":1,"task":"這是一個新任務2","completed":false}]
-// >>[{"id":1,"task":"這是一個新任務2","completed":true}]
+// 取得資料併存進Redux store
+// 儘管您可以直接在組件中使用 axios 呼叫 API 並處理回應，但將資料存儲在 Redux store 中，可以在應用程式中的任何組件中訪問存儲在 Redux 中的資料，而不需要將資料傳遞到組件層次結構中的每個層次。
 
-// 取得狀態
-// console.log(JSON.stringify(store.getState()));
+const gettingTasks = async () => {
+  try {
+    // 從後端API獲取資料
+    const response = await axios.get('/tasks');
 
-// store.dispatch(removeTask({ id: 1 }));
-// console.log(JSON.stringify(store.getState()));
-// >>[]
+    // 用store.dispatch()將資料傳遞給 Redux store，以通知 store 資料已經改變。
+    store.dispatch(
+      getTasks({
+        tasks: response.data,
+      }),
+    );
+  } catch (error) {
+    console.log(error);
+    store.dispatch({
+      type: 'SHOW_ERROR',
+      payload: {
+        error: error.message,
+      },
+    });
+  }
+};
 
-// 執行非同步處理
-// store.dispatch(fetchTodo(2));
-// store.dispatch(fetchTodo(3));
+gettingTasks();
 
-store.dispatch({
-  type: 'SHOW_ERROR', payload: {
-    error: '測試error中間件',
-  },
-});
-
-// 取消訂閱
-// unsubscribe();
 const Home = () => {
   return <div>Home</div>;
 };
