ec17d01f90a25200b65aded4da82188fa1edebf0
wwwchendev <wwwchendev@gmail.com>
2024-02-10 02:08:00 +0800

    使用createAsyncThunk呼叫API、設置了slice初始狀態以及extraReducers

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,34 +1,37 @@
 import axios from '@/utils/http';
 import store from '@/store/configureStore';
 
-import { getTasks } from '@/store/tasks';
+import { fetchTasks, getTasks } from '@/store/tasks';
 
 // 取得資料併存進Redux store
 // 儘管您可以直接在組件中使用 axios 呼叫 API 並處理回應，但將資料存儲在 Redux store 中，可以在應用程式中的任何組件中訪問存儲在 Redux 中的資料，而不需要將資料傳遞到組件層次結構中的每個層次。
 
-const gettingTasks = async () => {
-  try {
-    // 從後端API獲取資料
-    const response = await axios.get('/tasks');
+// 1.一般呼叫API的方法
+// const gettingTasks = async () => {
+//   try {
+//     // 從後端API獲取資料
+//     const response = await axios.get('/tasks');
 
-    // 用store.dispatch()將資料傳遞給 Redux store，以通知 store 資料已經改變。
-    store.dispatch(
-      getTasks({
-        tasks: response.data,
-      }),
-    );
-  } catch (error) {
-    console.log(error);
-    store.dispatch({
-      type: 'SHOW_ERROR',
-      payload: {
-        error: error.message,
-      },
-    });
-  }
-};
+//     // 用store.dispatch()將資料傳遞給 Redux store，以通知 store 資料已經改變。
+//     store.dispatch(
+//       getTasks({
+//         tasks: response.data,
+//       }),
+//     );
+//   } catch (error) {
+//     console.log(error);
+//     store.dispatch({
+//       type: 'SHOW_ERROR',
+//       payload: {
+//         error: error.message,
+//       },
+//     });
+//   }
+// };
+// gettingTasks();
 
-gettingTasks();
+// 2.使用redux-toolkit的createAsyncThunk呼叫API
+store.dispatch(fetchTasks());
 
 const Home = () => {
   return <div>Home</div>;
--- src/store/tasks.js
+++ src/store/tasks.js
@@ -1,54 +1,69 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-// [
-//   {
-//      id: state.length + 1,
-//      task: action.payload.task,
-//      completed: false,
-//   },{...},{...}
-// ]
+const initialState = {
+  tasks: [],
+  loading: false,
+  error: null,
+};
+
+// createAsyncThunk是Redux Toolkit 提供的非同步 action creator
+// 在 createSlice中可以使用 extraReducers 來處理這些非同步 action 的狀態，將非同步 action 的不同階段（例如 pending、fulfilled、rejected）與對應的 reducer 函式進行匹配，這樣就可以自動處理非同步 action 的狀態變化。
+export const fetchTasks = createAsyncThunk(
+  'fetchTasks',
+  async (payload, { rejectWithValue }) => {
+    // rejectWithValue 用於拒絕此非同步 action 並提供一個值給 reducer
+    try {
+      const response = await axios.get('http://localhost:5000/api/tasks');
+      return { tasks: response.data };
+    } catch (error) {
+      return rejectWithValue({ error: error.message });
+    }
+  },
+);
 
 // 使用createSlice可以同時創建Action和Reducer
 const taskSlice = createSlice({
   name: 'tasks',
-  initialState: [],
+  initialState: initialState,
   reducers: {
     getTasks: (state, action) => {
-      return action.payload.tasks;
+      state.tasks = action.payload.tasks;
     },
     // action: function
     addTask: (state, action) => {
-      state.push({
+      state.tasks.push({
         id: state.length + 1,
         task: action.payload.task,
         completed: false,
       });
     },
     removeTask: (state, action) => {
-      return state.filter(task => task.id !== action.payload.id);
+      return state.tasks.filter(task => task.id !== action.payload.id);
     },
     completeTask: (state, action) => {
-      const index = state.findIndex(task => task.id === action.payload.id);
+      const index = state.tasks.findIndex(
+        task => task.id === action.payload.id,
+      );
       if (index !== -1) {
-        state[index].completed = true;
+        state.tasks[index].completed = true;
       }
     },
   },
+  extraReducers: {
+    [fetchTasks.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [fetchTasks.fulfilled]: (state, action) => {
+      state.tasks = action.payload.tasks;
+      state.loading = false;
+    },
+    [fetchTasks.rejected]: (state, action) => {
+      state.error = action.payload.error;
+      state.loading = false;
+    },
+  },
 });
-
-export const fetchTodo = id => {
-  return async function (dispatch, getState) {
-    try {
-      const result = await fetch(
-        `https://jsonplaceholder.typicode.com/todos/${id}`,
-      );
-      const resultTask = await result.json();
-      dispatch(addTask({ task: resultTask.title }));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-};
 
 export const { getTasks, addTask, removeTask, completeTask } =
   taskSlice.actions;
