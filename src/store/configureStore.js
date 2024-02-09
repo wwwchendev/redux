cad3e7739e2013e22d61547ac694e3e5505a6410
wwwchendev <wwwchendev@gmail.com>
2024-02-09 11:21:43 +0800

    安裝@reduxjs/toolkit，使用configureStore取代createStore
    1. @reduxjs/toolkit讓開發者可以直接使用非同步操作而無需額外設定
    2. 可以將多個 reducer 直接作為物件屬性傳遞
    3. Redux Toolkit 整合了 Redux DevTools Extension，因此不需要手動設定來啟用它。

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,39 +1,13 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware, compose } from "redux"
-import { thunk } from 'redux-thunk'; // Redux thunk用於非同步請求處理
-import { devToolsEnhancer } from 'redux-devtools-extension'
-import taskReducer from '@/store/tasks'
+import { configureStore } from '@reduxjs/toolkit';
+import taskReducer from '@/store/tasks';
 // import userReducer from '@/store/users'
 
-const rootReducer = combineReducers({
-  tasks: taskReducer,
-  //user: userReducer, // 將 userReducer 也加入組合
+// @reduxjs/toolkit讓開發者可以直接使用非同步操作而無需額外設定
+const store = configureStore({
+  reducer: {
+    tasks: taskReducer,
+    //user: userReducer, // 將 userReducer 也加入組合
+  },
 });
-
-// 加入中間件:thunk
-//const store = createStore(rootReducer, applyMiddleware(thunk)); 
-
-// 1.使用devtools 基本store寫法
-// const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-// const store = createStore(rootReducer, 
-//     composeEnhancers(applyMiddleware(thunk))
-// );
-
-// 2.使用devtools 使用其他中間件的store寫法
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(rootReducer, 
-//     composeEnhancers(applyMiddleware(thunk))
-// );
-
-// 3.使用devtools並啟用trace
-const composeEnhancers = compose(
-  applyMiddleware(thunk),
-  devToolsEnhancer({ trace: true })
-);
-
-const store = createStore(
-  rootReducer,
-  // composeEnhancers(applyMiddleware(thunk)),
-  composeEnhancers
-);
 
 export default store;
